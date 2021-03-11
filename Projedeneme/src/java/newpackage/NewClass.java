590189262d00cb9229ce1a8489f1559de2145129
Sahranur Er <sahranurerr@gmail.com>
2021-03-11 14:13:04 +0300

    deneme
    deneneniyor

---
+++ Projedeneme/src/java/newpackage/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author lenovo
+ */
+public class NewClass {
+    private String a;
+
+    public NewClass(String a) {
+        this.a = a;
+    }
+    
+}
